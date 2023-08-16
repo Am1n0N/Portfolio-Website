46e84090bc784ef4a8f0c3c6a59ae4b64eac505a
Am1n0N <Mohamedaminerouissi26@gmail.com>
2023-08-16 14:29:37 +0100

    Added a Project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import healthhub from "../../Assets/Projects/healthhub.webp";
+import renter from "../../Assets/Projects/renter.webp";
 
 
 function Projects() {
@@ -24,9 +25,20 @@ function Projects() {
               title="healthhub"
               description="HealthHub is a web application that allows patients to book appointments, manage their patient records, receive remote consultations, manage their prescriptions, receive patient reminders, provide feedback, and monitor their health through a connected device."
               ghLink="https://github.com/TheCodeGlow/projectClinique"
-              demoLink="https://chatify-49.web.app/"
+              // demoLink="https://chatify-49.web.app/"
             />
           </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={renter}
+              isBlog={false}
+              title="Renter"
+              description="Renter is an Airbnb-style platform that allows users to easily list and book spaces for rent. Hosts can create listings for their properties with photos, descriptions, pricing and availability. Guests can search through listings, view details, book stays and communicate with hosts."
+              ghLink="https://github.com/Am1n0N/Renter-BookingApp"
+              // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+          
 
         </Row>
       </Container>
